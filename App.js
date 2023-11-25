9bc3f8cb898a2e0a7065ba9db55cd8ca551eec21
MiguelR222 <miguel.ruiz0652@gmail.com>
2023-11-25 02:55:19 -0600

    Removed imports

--- App.js
+++ App.js
@@ -1,10 +1,4 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView} from 'react-native';
-import { useState } from 'react';
-import MainScreen from './src/screens/MainScreen';
-import { useMovieGetList } from './src/hooks/useMovieGetList';
-import MoviesScreen from './src/screens/MoviesScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import Stacks from './src/components/Tabs'
 
