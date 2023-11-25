862f813cab7a6d5b73fd0358bec29e255d59d4b1
MiguelR222 <miguel.ruiz0652@gmail.com>
2023-11-25 02:33:27 -0600

    fixed imports, onPress, styles

--- src/screens/SeriesScreen.js
+++ src/screens/SeriesScreen.js
@@ -1,44 +1,52 @@
 import React, { useState } from 'react';
-import { FlatList, SafeAreaView, TouchableOpacity, Text, View, StyleSheet, StatusBar } from 'react-native';
+import { FlatList, SafeAreaView, View, StyleSheet} from 'react-native';
 import ListItem from '../components/ListItem';
 
 const SeriesScreen = ({ genres, navigation }) => {
   const [selectedGenre, setSelectedGenre] = useState(null);
+
   const renderItem = ({ item }) => (
     <ListItem
       id={item.id}
       name={item.name}
       navigation={navigation}
-      onPress={(genreID) => { setSelectedGenre(genreID); navigation.navigate('Result TV', { selectedGenre }) }}
+      onPress={() => {
+        setSelectedGenre(item.id);
+        navigation.navigate('Result TV', { selectedGenre: item.id }); 
+      }}
     />
-  )
-  console.log(selectedGenre)
+  );
+
+  console.log(selectedGenre);
+
   return (
     <SafeAreaView style={styles.container}>
-      <FlatList style= {styles.list}
+      <FlatList
+        style={styles.list}
         data={genres}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
         ItemSeparatorComponent={() => (
-          <View style={{ backgroundColor: "red", height: 2 }} />
-        )}/>
+          <View style={{ backgroundColor: 'red', height: 2 }} />
+        )}
+      />
     </SafeAreaView>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'powderblue'
+    backgroundColor: '#E0F7FA',
   },
   selectedItem: {
-    flex:1,
+    flex: 1,
     backgroundColor: 'red',
-    color:'white'
+    color: 'white',
   },
   list: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
 
-export default SeriesScreen
+export default SeriesScreen;
