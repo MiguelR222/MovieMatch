0c3c5339abbc19e208260626235be16d3603ea86
MiguelR222 <miguel.ruiz0652@gmail.com>
2023-11-25 02:31:02 -0600

    style changes

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,21 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 
-const ListItem = ({ id, name, onPress, isSelected, navigation}) => {
-  const itemStyle = isSelected ? styles.selectedItem : styles.item;
-
-  
-    return (
-      <View style={itemStyle}>
-        <TouchableOpacity onPress={()=> onPress(id)}>
-          <Text style={styles.movieData}>{id}  {name}</Text>
-        </TouchableOpacity>
+const ListItem = ({ id, name, onPress}) => {
+  return (
+    <TouchableOpacity style={styles.item} onPress={onPress}>
+      <View>
+        <Text style={styles.movieData}>{name}</Text>
       </View>
-    );
-  };
+    </TouchableOpacity>
+  );
+};
 
 const styles = StyleSheet.create({
-    item: {
+  item: {
+      borderRadius:30,
         padding: 20,
         marginVertical: 8,
         marginHorizontal: 16,
